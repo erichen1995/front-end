fafffedb9b6f2f6b7b0204c398b3fa18798a8a55
erichen1995 <1366343126@qq.com>
2020-11-30 11:48:51 +0800

    更新文件夹

---
+++ Homework/practice/JS/print-triangle.js
@@ -0,0 +1,12 @@
+function print_triangle(num) {
+  for(var i = 1; ;i++) {
+    const print = "";
+    if(num - i <= i -1) {
+      console.log('#')
+      break;
+    }
+    print = "#";
+  }
+
+}
+print_triangle(21);
